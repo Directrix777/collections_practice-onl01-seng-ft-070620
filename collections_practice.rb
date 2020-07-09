e09786519e2565059d4c1d27eb644e9bbfbab8c3
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-09 15:01:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,50 @@
+require 'pry'
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort{|a,b| (b <=> a)}
+end
+
+def sort_array_char_count(array)
+  array.sort{|a,b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array.sort{|a,b|
+    if array.index(a) == 1 && array.index(b) == 2
+      1
+    else
+      -1
+    end}
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+
+end
+
+def find_a(array)
+  array.collect do |word|
+    word if word.chr == "a"
+  end
+end
+
+def sum_array(array)
+  total = 0
+  array.each do |num|
+    total += num
+  end
+  total
+end
+
+def add_s(array)
+  array.collect do |word|
+    word += "s" unless array.index(word) == 1
+    word
+    end
+end
